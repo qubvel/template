3b9be6e45628746e5796d69af74b6fe5300e5ec4
Pavel <31920396+qubvel@users.noreply.github.com>
2018-08-23 17:23:17 +0300

    fixed

--- docker_run.sh
+++ docker_run.sh
@@ -9,8 +9,8 @@ eval $(parse_yaml config.yaml "config_")
 nvidia-docker run -d \
     -p $config_jupyter_port:8888 \
     -p $config_tensorboard_port:6006 \
-    --name $config_docker_image_name \
+    --name $config_docker_container_name \
     -v $(pwd):/home/user/project/ \
     -e "UID=$(id -u)" \
     -e "GID=$(id -g)" \
-    $config_docker_container_name
+    $config_docker_image_name
